d0fbd632dea030166359dd56ec8ce6ec725f441a
ThyagoRafael <thyagorafael57@gmail.com>
2026-01-05 09:37:57 -0300

    feat: criar header e footer padrões para páginas de autenticação

---
+++ frontend/src/components/footer/AuthFooter/index.tsx
@@ -0,0 +1,9 @@
+import styles from "./AuthFooter.module.css";
+
+export default function AuthFooter() {
+	return (
+		<footer className={styles.container}>
+			<p className={styles.text}>&copy;2025 Davies</p>
+		</footer>
+	);
+}
---
+++ frontend/src/components/header/AuthHeader/index.tsx
@@ -0,0 +1,13 @@
+import styles from "./AuthHeader.module.css";
+import logo from "../../../assets/logo.png";
+import { Link } from "react-router-dom";
+
+export default function AuthHeader() {
+	return (
+		<header className={styles.container}>
+			<Link to="/" className={styles.link}>
+				<img src={logo} alt="Logo Davies" />
+			</Link>
+		</header>
+	);
+}
