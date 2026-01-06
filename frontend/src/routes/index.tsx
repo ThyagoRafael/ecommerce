da13a48f7e1259676d87f235a5f0a4b040829335
ThyagoRafael <thyagorafael57@gmail.com>
2026-01-06 11:22:55 -0300

    feat: adicionar rotas das páginas de cadastro e login de usuário com layout padrão

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,17 +1,26 @@
 import { Routes, Route } from "react-router-dom";
-import Login from "../pages/Login";
-import Register from "../pages/Register";
+import Login from "../pages/Auth/Login";
+import Register from "../pages/Auth/Register";
 import Home from "../pages/Home";
 import AuthLayout from "../templates/AuthLayout";
 
 export default function Router() {
 	return (
 		<Routes>
-			<Route index element={<Home />} />
+			<Route
+				index
+				element={<Home />}
+			/>
 
 			<Route element={<AuthLayout />}>
-				<Route path="login" element={<Login />} />
-				<Route path="register" element={<Register />} />
+				<Route
+					path="login"
+					element={<Login />}
+				/>
+				<Route
+					path="register"
+					element={<Register />}
+				/>
 			</Route>
 		</Routes>
 	);
