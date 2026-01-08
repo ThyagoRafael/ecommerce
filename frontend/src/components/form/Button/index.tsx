c587f8d8404f5a31d3370b7891c7ccf6fa182e3b
ThyagoRafael <thyagorafael57@gmail.com>
2026-01-08 09:01:03 -0300

    chore: adicionar atributo disabled no button

--- frontend/src/components/form/Button/index.tsx
+++ frontend/src/components/form/Button/index.tsx
@@ -4,13 +4,15 @@ import styles from "./Button.module.css";
 interface ButtonProps {
 	children: string;
 	handleClick: (e: FormEvent) => void;
+	disabled: boolean;
 }
 
-export default function Button({ children, handleClick }: ButtonProps) {
+export default function Button({ children, handleClick, disabled }: ButtonProps) {
 	return (
 		<button
 			className={styles.button}
 			onClick={handleClick}
+			disabled={disabled}
 		>
 			{children}
 		</button>
