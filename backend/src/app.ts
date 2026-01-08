59a726700b3dd05a272303e4a22b5f141a90fa88
ThyagoRafael <thyagorafael57@gmail.com>
2026-01-08 08:53:30 -0300

    feat: implementar cors com origem da aplicação

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,6 +1,7 @@
 import express from "express";
 import { routes } from "./routes/index.js";
 import { errorMiddleware } from "./middlewares/error.middleware.js";
+import cors from "cors";
 
 class App {
 	server: express.Express;
@@ -13,6 +14,7 @@ class App {
 	}
 
 	middlewares() {
+		this.server.use(cors({ origin: "http://localhost:5173" }));
 		this.server.use(express.json());
 	}
 
