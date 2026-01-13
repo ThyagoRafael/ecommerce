65f7a10a9d21c441384cff1e0adf58a774d38616
ThyagoRafael <thyagorafael57@gmail.com>
2026-01-13 19:04:46 -0300

    feat: adicionar role no token e desenvolver middleware para autorização de usuário

--- backend/src/middlewares/auth.middleware.ts
+++ backend/src/middlewares/auth.middleware.ts
@@ -1,15 +1,15 @@
 import { type Request, type Response, type NextFunction } from "express";
 import { AppError } from "../errors/AppError.js";
-import { validateToken } from "../helpers/jwt.js";
+import { validateToken } from "../helpers/auth.js";
 
-export async function authMiddleware(req: Request, res: Response, next: NextFunction) {
-	const authHeaderExists = req.headers.authorization;
+export async function authenticationMiddleware(req: Request, res: Response, next: NextFunction) {
+	const authHeader = req.headers.authorization;
 
-	if (!authHeaderExists) {
+	if (!authHeader) {
 		throw new AppError("O token não foi enviado", 404);
 	}
 
-	const [, token] = authHeaderExists.split(" ");
+	const [, token] = authHeader.split(" ");
 
 	if (!token) {
 		throw new AppError("O token está mal formatado", 400);
@@ -18,9 +18,23 @@ export async function authMiddleware(req: Request, res: Response, next: NextFunc
 	const decodedToken = await validateToken(token);
 
 	req.user = {
-		...req.user,
 		id: decodedToken.userId,
+		role: decodedToken.role,
 	};
+
+	next();
+}
+
+export async function authorizationMiddleware(req: Request, res: Response, next: NextFunction) {
+	const userRole = req.user?.role;
+
+	if (!userRole) {
+		throw new AppError("Role não fornecida", 404);
+	}
+
+	if (userRole !== "admin") {
+		throw new AppError("Usuário não tem permissão para acessar esse recurso");
+	}
 
 	next();
 }
