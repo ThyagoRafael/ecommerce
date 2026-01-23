521a4879f45a2f0c62aea2a67ab7daa28763362e
ThyagoRafael <thyagorafael57@gmail.com>
2026-01-23 09:03:06 -0300

    chore: alterar nome e estrutura das pastas para padronização

--- frontend/src/components/footer/Footer/index.tsx
+++ frontend/src/components/footer/Footer/index.tsx
@@ -1,6 +1,6 @@
-import styles from "./AuthFooter.module.css";
+import styles from "./Footer.module.css";
 
-export default function AuthFooter() {
+export default function Footer() {
 	return (
 		<footer className={styles.container}>
 			<p className={styles.text}>&copy;2025 Davies</p>
