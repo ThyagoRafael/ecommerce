d3f947635935d6823c3d7db5881692120dbf7f68
ThyagoRafael <thyagorafael57@gmail.com>
2026-01-23 08:39:30 -0300

    feat: adicionar layout principal nas rotas principais da aplicação

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -3,14 +3,23 @@ import Login from "../pages/Auth/Login";
 import Register from "../pages/Auth/Register";
 import Home from "../pages/Home";
 import AuthLayout from "../templates/AuthLayout";
+import MainLayout from "../templates/MainLayout";
+import ProductDetails from "../pages/ProductDetails";
 
 export default function Router() {
 	return (
 		<Routes>
-			<Route
-				index
-				element={<Home />}
-			/>
+			<Route element={<MainLayout />}>
+				<Route
+					index
+					element={<Home />}
+				/>
+
+				<Route
+					path="produtos/:productId"
+					element={<ProductDetails />}
+				/>
+			</Route>
 
 			<Route element={<AuthLayout />}>
 				<Route
