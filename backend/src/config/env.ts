54db24612e41395f0fc545bbb9d8d4a70538213c
ThyagoRafael <thyagorafael57@gmail.com>
2026-02-01 10:19:54 -0300

    feat: adicionar variáveis de ambiente do cloudinary para verificação

--- backend/src/config/env.ts
+++ backend/src/config/env.ts
@@ -5,6 +5,9 @@ const envSchema = z.object({
 	PORT: z.string().default("3000"),
 	DATABASE_URL: z.string(),
 	JWT_SECRET: z.string(),
+	CLOUDINARY_NAME: z.string(),
+	CLOUDINARY_KEY: z.string(),
+	CLOUDINARY_SECRET: z.string(),
 });
 
 export const env = envSchema.parse(process.env);
